12509538aef0aae4666b06c1a33e5af9ea495f15
subhra2323 <subhrakumari2323@gmail.com>
2026-09-20 16:50:45 +0530

    Added Day 1 Python basics

---
+++ day1_basics.py
@@ -0,0 +1,34 @@
+# Day 1: Python Basics
+
+# Variables
+name = "Rupam"
+age = 23
+
+print(name)
+print(age)
+
+# Data Types
+a = 10
+b = 10.5
+c = "Python"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# String Indexing
+language = "Python"
+
+print(language[0])
+print(language[-1])
+
+# String Slicing
+print(language[0:3])
+
+# Type Conversion
+number = 12
+text = str(number)
+
+print(type(text))
